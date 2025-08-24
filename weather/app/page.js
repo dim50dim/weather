60391a6ebfb2508f2d998afa423042def4049149
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-24 18:09:23 +0200

    does not work Image component.

--- weather/app/page.js
+++ weather/app/page.js
@@ -1,5 +1,5 @@
 "use client"
-
+import Image from 'next/image';
 import axios from "axios";
 
 import { useState } from "react";
@@ -28,7 +28,12 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${AP
 
   return (
     <div>
-          <button onClick={fetchWeather}>Fetch data</button>
+     <Image
+        src="https://images.unsplash.com/photo-1561470508-fd4df1ed90b2?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.1.0"
+        alt="Background"
+        fill
+        className="object-cover"
+      />
     </div>
   );
 }
