1a3735bf1fb48a151c253e998a085c5ca056b5dd
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-24 16:31:15 +0200

    implemented three states , also made import from react-icons. BsSearch.

--- weather/app/page.js
+++ weather/app/page.js
@@ -1,7 +1,18 @@
+import axios from "axios";
+
+import { useState } from "react";
+import {BsSearch} from "react-icons";
+
 
 
 export default function Home() {
+  const [city,setCity] = useState('')
+  const [weather,setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=dublin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  
+  
   return (
     <div>
           
