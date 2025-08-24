47573a1010ea7db11ff790406bab103054cebf76
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-24 15:01:13 +0200

    created const with NEXT_PUBLIC_KEY

--- weather/app/page.js
+++ weather/app/page.js
@@ -1,6 +1,7 @@
 
 
 export default function Home() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${process.env.NEXT_PUBLIC_KEY}`
   return (
     <div>
           
