706ad6296b8b19ddddf8a96c5613fdb54b78c673
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-24 18:25:41 +0200

    styled a bit button search

--- weather/app/page.js
+++ weather/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import axios from "axios";
 
 import { useState } from "react";
-import {BsSearch} from "react-icons";
+import {BsSearch} from "react-icons/bs";
 
 
 
@@ -28,12 +28,23 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${AP
 
   return (
     <div>
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]'>
      <Image
         src="https://images.unsplash.com/photo-1561470508-fd4df1ed90b2?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.1.0"
         alt="Background"
         fill
         className="object-cover"
       />
+      </div>
+                  {/* search  */}
+                    <div className='relative flex justify-between items-center max-w-[500px] m-auto z-10 text-white'>
+                         <form >
+                           <div>
+                            <input type="text" placeholder='Search city' />
+                           </div>
+                           <button><BsSearch /></button>
+                         </form>
+                    </div>
     </div>
   );
 }
