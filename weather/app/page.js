5b4184ab334e39448d9fbb6e4399c7dd96d3ecc9
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-24 16:34:07 +0200

    made a function named fetchWeather - where used e.preventDefault() to avoid reload page, also setLoading() and axios with response

--- weather/app/page.js
+++ weather/app/page.js
@@ -12,7 +12,16 @@ export default function Home() {
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=dublin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
   
-  
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then.apply((response) => {
+         setWeather(response.data);
+         console.log(response.data);
+         
+    })
+  }
+
   return (
     <div>
           
