31b348969e0a8a9ef2130d5ed568459b612935a2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-24 19:56:16 +0200

    changed the main image.

--- weather/app/page.js
+++ weather/app/page.js
@@ -30,7 +30,7 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${AP
     <div>
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]'>
      <Image
-        src="https://images.unsplash.com/photo-1561470508-fd4df1ed90b2?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.1.0"
+        src="https://images.unsplash.com/photo-1693910176136-b06d8326f0e7?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="Background"
         fill
         className="object-cover"
