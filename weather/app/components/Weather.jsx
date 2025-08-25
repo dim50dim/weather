069d516344b726dd87503d031288ef1fffeed5d2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-08-25 17:34:39 +0200

    styled a bit Weather component.

--- weather/app/components/Weather.jsx
+++ weather/app/components/Weather.jsx
@@ -7,11 +7,19 @@ const Weather = ({data}) => {
     console.log(data);
     
   return (
-    <div>
-      <div>
-        <div>
-          <Image  alt='/' />
+    
+    <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] p-4 m-auto text-gray-300 z-10'>
+      <div className='relative flex justify-between pt-12'>
+        <div className='flex flex-col items-center'>
+             <Image
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            alt='/'
+            width='100'
+            height='100'
+          />
+          <p> {data.weather[0].main} </p>
         </div>
+        <p> {data.main.temp.toFixed(0)}&#176; </p>
       </div>
     </div>
   )
